bf85be5a03a25d0d66e4008dca8faebd6499493e
Leon Wright <wrightleon@icloud.com>
2016-10-11 11:39:31 -0500

    Changes for server compatibility

--- js/required.js
+++ js/required.js
@@ -6,7 +6,7 @@ var brandSelect = $('#brandSelect');
 
 $.ajax({
    type: 'GET',
-    url: 'http://localhost:8000/api/v1/ads/brands/all/',
+    url: 'http://45.55.79.166/api/v1/ads/brands/all/',
     success: function (data) {
         $.each(data, function (i, brand) {
             brandSelect.append('<option value='+brand.id+'>'+brand.name+'</option>');
